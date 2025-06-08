855f942c9b1494f82504468ee0e83af789f7d2a6
Ryan Eka <ryanzzeka@gmail.com>
2025-06-09 00:24:55 +0700

    penambahan fitur untuk get, create, update, delete user

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -15,6 +15,49 @@ class UserController {
             next(err)
         }
     }
+
+    async getById(req, res, next) {
+        try {
+            const user = await UserService.getById();
+            if (!user) throw new NotFound("Data User Belum Ada!");
+            res.json({
+                success: true, 
+                message: "User Berhasil Di dapat!", 
+                data: users 
+            })
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    async ceate(req, res, next) {
+        try {
+            const user = await UserService.create(req.body);
+            res.status(200).json({success: true, message: "User Berhasil Dibuat", data: user})
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    async update(req, res, next) {
+        try {
+            const user = await UserService.update(req.params.id, req.body);
+            if (!user) throw new NotFound("Data User Tidak Ditemukan")
+            res.status(200).json({success: true, message: "User Berhasil Diupdate", data: user})
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    async delete(req, res, next) {
+        try {
+            const user = await UserService.delete(req.params.id);
+            if (!user) throw new NotFound("Data User Tidak Ditemukan")
+            res.status(200).json({success: true, message: "User Berhasil Dihapus", data: user})
+        } catch (err) {
+            next(err)
+        }
+    }
 }
 
 module.exports = new UserController();
--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -1,5 +1,8 @@
 const { User } = require('../../../models');
 const bcrypt = require('bcrypt');
+const BedRequestError = require('../../errors/BedRequestError');
+const NotFound = require('../../errors/NotFoundError');
+const ServerError = require('../../errors/ServerError');
 
 class UserService {
     constructor() {
@@ -8,6 +11,69 @@ class UserService {
 
     async getAll() {
         return await User.findAll({ attributes: { exclude: ['password']}})
+    }
+
+    async getById(id) {
+        return await User.findByPk(id, { attributes: { exclude: ['password']}});
+    }
+
+    async create(data) {
+        const existingUser = await User.findOne({
+            where: {email: data.email}
+        });
+
+        if (existingUser) {
+            throw new BedRequestError("Email Sudah Terdaftar");
+        }
+
+        const hash = await bcrypt.hash(data.password, this.SALT_ROUNDS);
+
+        const user = await User.create({ ...data, password: hash});
+        const userJson = user.toJSON();
+        delete userJson.password;
+
+        return userJson;
+    }
+
+    async update(id, data) {
+        const user = await User.findByPk(id)
+        if (!user) return null;
+
+        if (data.email && data.email !== user.email) {
+            const existingUser = await User.findOne({
+                where: {email: data.email}
+            })
+
+            if (existingUser) {
+                throw new BedRequestError("Email Sudah Dipake Brow!")
+            }
+        }
+
+        if (data.password) {
+            data.password = await bcrypt.hash(data.password, this.SALT_ROUNDS);
+        } else {
+            delete data.password;
+        }
+
+        try {
+            await user.update(data, {validate: true});
+        } catch (err) {
+            console.error("error", err);
+            const message = err.errors?.map(e => e.message) || [err.message];
+            throw new ServerError("Gagal update user: " + message.join(', '));
+        }
+
+        const userJson = user.toJSON();
+        delete userJson.password;
+        return userJson;
+    }
+
+    async delete(id) {
+        const user = await User.findByPk(id);
+        if (!user) throw new NotFound("User tidak ditemukan!");
+        
+        await user.destroy();
+        return true;
     }
 }
 
